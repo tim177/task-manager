77ecd5f81983f3a5a0c0e674ee9b1d61ea3ccfa4
tim177 <timaraw18@gmail.com>
2025-04-15 00:16:50 +0530

    added allowedhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true, // Binds the preview server to all network interfaces
+    port: 4173, // Make sure you're using the same port as your local environment
+    allowedHosts: ["task-manager-frontend-ql3c.onrender.com"], // Allow the Render domain
+  },
 });
